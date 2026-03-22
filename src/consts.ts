09f6446ed41281e7da671a3a525d449eb4202d0e
OsGa <oscarhuang950324@gmail.com>
2026-03-22 16:24:08 +0800

    feat: first init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "osga.dev/now";
+export const SITE_DESCRIPTION = "What I'm doing now.";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,17 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const entries = await getCollection('monthly');
+  entries.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: entries.map((entry) => ({
+      title: entry.data.title,
+      description: entry.data.description,
+      pubDate: entry.data.pubDate,
+      link: `/${entry.id}/`,
+    })),
+  });
 }
